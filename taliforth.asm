b4b53d9cc45862e161e38c5713664ca0b426b471
Scot W. Stevenson <scot.stevenson@gmail.com>
2017-12-12 11:09:51 +0100

    Added first INTERPRET code

--- taliforth.asm
+++ taliforth.asm
@@ -1,7 +1,7 @@
 ; Tali Forth 2 for the 65c02
 ; Scot W. Stevenson <scot.stevenson@gmail.com>
 ; First version: 19. Jan 2014 (Tali Forth)
-; This version: 07. Dec 2017
+; This version: 12. Dec 2017
 
 ; This is the main file for Tali Forth 2
 
@@ -121,15 +121,130 @@ _nibble_to_ascii:
 
 
 interpret:
+.scope
         ; """Core routine for the interpreter called by EVALUATE and QUIT.
         ; Process one line only. Assumes that the address of name is in
         ; cib and the length of the whole input line string is in ciblen
         ; """
-        ; TODO 
-                lda #'i                 ; TODO for testing
-                jsr emit_a
+                ; Normally we would use PARSE here with the SPACE character as
+                ; a parameter (PARSE replaces WORD in modern Forths). However,
+                ; Gforth's PARSE-NAME makes more sense as it uses spaces as
+                ; delimiters per default and skips any leading spaces, which
+                ; PARSE doesn't
+_loop:
+                jsr xt_parse_name       ; ( "string" -- addr u ) 
+
+                ; If PARSE-NAME returns 0 (empty line), no characters were left
+                ; in the line and we need to go get a new line
+                lda 0,x                 ; force flag check
+                beq _line_done
+     
+                ; Go to FIND-NAME to see if this is a word we know. We have to
+                ; make a copy of the address in case it isn't a word we know and
+                ; we have to go see if it is a number
+                jsr xt_two_dup          ; TODO convert this to assembler
+                jsr xt_find_name        ; ( addr u -- nt | 0 )
+
+                ; a zero signals that we didn't find a word in the Dictionary
+                lda 0,x
+                bne _got_name_token
+
+                ; We didn't get any nt we know of, so let's see if this is
+                ; a number. 
+                inx             ; drop
+                inx
+
+                ; If the number conversion doesn't work, NUMBER will do the
+                ; complaining for us
+                jsr xt_number           ; ( addr u -- u | d ) 
+
+                ; If we're interpreting, we're done
+                lda state
+                beq _loop
+
+                ; We're compiling, so there is a bit more work. Note this
+                ; doesn't work with double-cell numbers, only single-cell
+                lda #<literal_rt        ; TODO check if LSB or MSB first
+                pha
+                lda #>literal_rt
+                pha
+                jsr cmpl_subroutine
+
+                ; compile our number
+                ; TODO convert this to assembler
+                jsr xt_comma
+     
+                ; That was so much fun, let's do it again!
+                bra _loop
+
+_got_name_token:
+                ; We have a known word's nt as TOS. We're going to need its xt
+                ; though, which is four bytes father down. 
+
+                ; Arrive here with ( addr u nt ), so we NIP twice
+                jsr xt_nip      ; TODO convert to assembler
+                jsr xt_nip 
+                
+                ; This is a quicker
+                ; version of NAME>INT. But first, save a version of nt for
+                ; error handling and compilation stuff.
+                lda 0,x
+                sta tmpbranch
+                lda 1,x
+                sta tmpbranch+1
+
+                jsr xt_name_to_int
+
+                ; See if we are in interpret or compile mode
+                lda state
+                bne _compile
+
+                ; We are interpreting, so EXECUTE the xt that is TOS. First,
+                ; though, see if this isn't a compile-only word, which would be
+                ; illegal. The status byte is the second one of the header.
+                ldy #1
+                lda (tmpbranch),y
+                and #CO                 ; mask everything but Compile Only bit
+                beq _interpret
+
+                ; TODO see if we can print offending word first
+                lda #1                  ; code for "compile only word" error
+                jmp error
+
+_interpret:
+                ; We JSR to EXECUTE instead of calling the xt directly because
+                ; the RTS of the word we're executing will bring us back here,
+                ; skipping EXECUTE completely during RTS. If we were to execute
+                ; xt directly, we have to fool around with the Return Stack
+                ; instead, which is actually slightly slower
+                jsr xt_execute
+
+                ; That's quite enough for this word, let's get the next one
+                jmp _loop
+
+_compile:
+                ; We're compiling! However, we need to see if this is an
+                ; IMMEDIATE word, which would mean we execute it right now even
+                ; during compilation mode. Fortunately, we saved the nt so life
+                ; is easier. The flags are in the second byte of the header
+                ldy #1
+                lda (tmpbranch),y
+                and #IM                 ; Mask all but IM bit
+                bne _interpret          ; IMMEDIATE word, execute right now
+
+                ; Compile the xt into the Dictionary with COMPILE,
+                jsr xt_compile_comma
+                jmp _loop
+
+_line_done:
+                ; drop stuff from PARSE_NAME
+                inx
+                inx
+                inx
+                inx
 
                 rts
+.scend
 
 
 error: 
