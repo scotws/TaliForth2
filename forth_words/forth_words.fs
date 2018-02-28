c25cbec6bd2458737c3d42c8343964ddfbda034b
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-02-28 12:04:32 +0100

    First modifications (incomplete)

---
+++ forth_words/forth_words.fs
@@ -0,0 +1,65 @@
+\ List of high-level Forth words for Tali Forth 2 for the 65c02
+\ Scot W. Stevenson <scot.stevenson@gmail.com>
+\ First version: FEHLT
+\ This version: 27. Feb 2018
+
+\ When changing these words, edit them here and then use the 
+\ forth_to_dotbyte.py tool to convert them to the required format
+\ for inclusion in Ophis. See forth_words/README.md for details
+
+\ Output and comment. Because it still blows my mind that we can
+\ define this stuff this simply 
+        : ( [char] ) parse 2drop ; immediate
+        : .( [char] ) parse type ; immediate
+
+\ Flow control. Some of these could be realized with CS-ROLL and
+\ CS-PICK instead, which seems to be all the rage these days.
+        : if postpone 0branch here 0 , ; immediate compile-only
+        : then here swap ! ; immediate compile-only
+        : else postpone branch here 0 , here rot ! ; immediate compile-only
+        : repeat postpone again here swap ! ; immediate compile-only
+        : until postpone 0branch , ; immediate compile-only
+        : while postpone 0branch here 0 , swap ; immediate compile-only
+
+\ DEFER and friends. Code taken from ANSI Forth specification. Some of
+\ these will be moved to assembler code in due course
+        : defer! >body ! ;
+        : defer@ >body @ ;
+        : is state @ if postpone ['] postpone defer! else ' defer! then ; immediate
+        : action-of state @ if postpone ['] postpone defer@ else ' defer@ then ; immediate
+
+\ High level math definitions. The should be moved to actual 65c02 code
+\ for speed at some point. Note we use SM/REM instead of FM/MOD for most
+\ stuff
+        : / >r s>d r> sm/rem swap drop ;
+        : /mod >r s>d r> sm/rem ;
+        : mod /mod drop ;
+        : */ >r m* r> sm/rem swap drop ;
+        : */mod >r m* r> sm/rem ;
+
+\ Output definitions. Since these usually involve the user, and humans
+\ are slow, these can stay high-level for the moment. However, in this
+\ state they don't check for underflow. Based on
+\ https://github.com/philburk/pforth/blob/master/fth/numberio.fth
+        : u. 0 <# #s #> type space ;
+        : u.r >r 0 <# #s #> r> over - spaces type ;
+        : .r >r dup abs 0 <# #s rot sign #> r> over - spaces type ;
+        : ud. <# #s #> type space ;
+        : ud.r >r <# #s #> r> over - spaces type ;
+        : d. tuck dabs <# #s rot sign #> type space ;
+        : d.r >r tuck dabs <# #s rot sign #> r> over - spaces type ;
+
+\ Temporary high-level words. TODO convert these to assembler
+        : to ( n -- 'name') ' >body ! ;
+        : within ( n1 n2 n3 -- f ) rot tuck > -rot > invert and ;
+        : /string ( addr u n -- addr u ) rot over + rot rot - ;
+
+\ Splash strings. We leave these as high-level words because they are
+\ generated at the end of the boot process and signal that the other
+\ high-level definitions worked (or at least didn't crash)
+        .( Tali Forth 2 for the 65c02)
+        cr .( Version ALPHA 27. Feb 2018)
+        cr .( Copyright 2014-2018 Scot W. Stevenson)
+        cr .( Tali Forth 2 comes with absolutely NO WARRANTY)
+        cr .( Type 'bye' to exit) cr
+\ END
---
+++ forth_words/user_words.fs
@@ -0,0 +1,150 @@
+\ List of optional Forth words for Tali Forth 2 for the 65c02
+\ Scot W. Stevenson <scot.stevenson@gmail.com>
+\ First version: FEHLT
+\ This version: 27. Feb 2018
+
+\ When changing these words, edit them here and then use the 
+\ forth_to_dotbyte.py tool to convert them to the required format
+\ for inclusion in Ophis. See forth_words/README.md for details
+
+\ Only uncomment the words you want to actually have included in Tali Forth 2.
+\ By default, that will be SEE.
+
+\ Note that these programs are not necessarily in the public domain,
+\ see the original sources for details
+
+\ -------------------------------------------------------
+\ SEE gives us information on a Forth word. At some point, this
+\ can be added to the native words to save on size
+        : see parse-name find-name dup 0= abort" No such name" 
+        base @ >r  hex  dup cr space ."  nt: " u.
+        dup 4 + @ space ." xt: " u. "
+        dup 1+ c@ 1 and if space ." CO " then
+        dup 1+ c@ 2 and if space ." AN " then
+        dup 1+ c@ 4 and if space ." IM " then
+        dup 1+ c@ 8 and if space ." NN " then
+        dup cr space ." size (decimal): " decimal wordsize dup .
+        swap name>int swap hex cr space dump  r> base ! ;
+
+\ -------------------------------------------------------
+\ WORDS&SIZES prints all known words and the sizes of their codes
+\ in bytes. It can be used to test the effects of different native
+\ compile parameters
+        \ : words&sizes  latestnt begin dup 0<> while dup name>string
+        \ type space  dup wordsize u. cr  2 + @ repeat drop ;
+
+\ -------------------------------------------------------
+\ FIBONACCI from
+\ https://atariwiki.org/wiki/Wiki.jsp?page=Forth%20Benchmark
+\ See also http://cubbi.com/fibonacci/forth.html
+\ TODO replace by a version that prints them one-by-one
+        \ : fib  ( n1 -- n2 )
+        \          dup 2 < if drop 1 exit then 
+        \          dup 1- recurse 
+        \          swap 2 - recurse + ;
+        
+\ -------------------------------------------------------
+\ FACTORIAL from 
+\ https://www.complang.tuwien.ac.at/forth/gforth/Docs-html/Recursion-Tutorial.html
+        \ : fact ( n -- n! )
+        \  dup 0> if
+        \     dup 1- recurse * else
+        \     drop 1 then ;
+
+\ -------------------------------------------------------
+\ PRIMES from 
+\ https://www.youtube.com/watch?v=V5VGuNTrDL8 (Forth Freak)
+        \  : primes ( n -- )
+        \      2 . 3 .
+        \      2 swap 5 do
+        \           dup dup * i < if 1+ then
+        \            1 over 1+ 3 do
+        \                  j i mod 0= if 1- leave then
+        \            2 +loop
+        \            if i . then
+        \      2 +loop
+        \    drop ;
+        
+\ -------------------------------------------------------
+\ MANDELBROT by Martin Heermance
+\ https://github.com/Martin-H1/Forth-CS-101/blob/master/mandelbrot.fs
+\ http://forum.6502.org/viewtopic.php?f=9&t=3706
+\ https://www.youtube.com/watch?v=fVa3Fx7dwBM
+        \  setup constants to remove magic numbers to allow
+        \  for greater zoom with different scale factors
+        \  20 constant maxiter "
+        \ -39 constant minval "
+        \  40 constant maxval "
+        \  20 5 lshift constant rescale "
+        \ rescale 4 * constant s_escape "
+        \  "
+        \ ( these variables hold values during the escape calculation ) "
+        \ variable creal "
+        \ variable cimag "
+        \ variable zreal "
+        \ variable zimag "
+        \ variable count "
+        \  "
+        \ ( compute squares, but rescale to remove extra scaling factor) "
+        \ : zr_sq zreal @ dup rescale */ ; "
+        \ : zi_sq zimag @ dup rescale */ ; "
+        \  "
+        \ ( translate escape count to ascii greyscale )"
+        \ : .char "
+        \          s\" ..,'~!^:;[/<&?oxox#  \" "
+        \          drop + 1 "
+        \          type ; "
+        \  "
+        \ ( numbers above 4 will always escape, so compare to a scaled value) "
+        \ : escapes? s_escape > ; "
+        \  "
+        \ ( increment count and compare to max iterations) "
+        \ : count_and_test? "
+        \          count @ 1+ dup count ! "
+        \          maxiter > ; "
+        \  "
+        \ ( stores the row column values from the stack for the escape calculation) "
+        \ : init_vars "
+        \          5 lshift dup creal ! zreal ! "
+        \          5 lshift dup cimag ! zimag ! "
+        \          1 count ! ; "
+        \  "
+        \ ( performs a single iteration of the escape calculation) "
+        \ : doescape "
+        \          zr_sq zi_sq 2dup + "
+        \          escapes? if "
+        \          2drop "
+        \          true "
+        \          else "
+        \          - creal @ +   ( leave result on stack ) "
+        \          zreal @ zimag @ rescale */ 1 lshift "
+        \          cimag @ + zimag ! "
+        \          zreal !                   ( store stack item into zreal ) "
+        \          count_and_test? "
+        \          then ; "
+        \  "
+        \ ( iterates on a single cell to compute its escape factor) "
+        \ : docell "
+        \          init_vars "
+        \          begin "
+        \          doescape "
+        \          until "
+        \          count @ "
+        \          .char ; "
+        \  "
+        \ ( for each cell in a row) "
+        \ : dorow "
+        \          maxval minval do "
+        \          dup i "
+        \          docell "
+        \          loop "
+        \          drop ; "
+        \  "
+        \ ( for each row in the set) "
+        \ : mandelbrot "
+        \          cr "
+        \          maxval minval do "
+        \          i dorow cr "
+        \          loop ; "
+        
+\ END 
