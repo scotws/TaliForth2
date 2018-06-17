b7baec3eaf6b0783a6cc3cdea0dc0a8f596481e0
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-06-17 13:19:10 +0200

    Fix handling of empty strings, closes #64

--- forth_code/forth_words.fs
+++ forth_code/forth_words.fs
@@ -58,7 +58,7 @@
 \ generated at the end of the boot process and signal that the other
 \ high-level definitions worked (or at least didn't crash)
         .( Tali Forth 2 for the 65c02)
-        cr .( Version ALPHA 16. June 2018 )
+        cr .( Version ALPHA 17. June 2018 )
         cr .( Copyright 2014-2018 Scot W. Stevenson)
         cr .( Tali Forth 2 comes with absolutely NO WARRANTY)
         cr .( Type 'bye' to exit) cr
