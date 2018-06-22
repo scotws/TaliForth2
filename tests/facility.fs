875de79a3e9940466d413c8550eb476374374ecd
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-06-22 11:49:33 +0200

    Oops - forgot to add files

---
+++ tests/facility.fs
@@ -0,0 +1,5 @@
+\ ------------------------------------------------------------------------
+testing facility words: at-xy page 
+
+( TODO AT-XY test missing )
+( TODO PAGE test missing )
---
+++ tests/tools.fs
@@ -0,0 +1,12 @@
+\ ------------------------------------------------------------------------
+testing tools words: .s ? dump name>string see state words
+
+\ Test for BYE not implemented
+
+( TODO .S test missing )
+( TODO ? test missing )
+( TODO DUMP test missing )
+( TODO NAME>STRING missing )
+( TODO SEE test missing )
+( TODO STATE test missing )
+( TODO WORDS test missing )
