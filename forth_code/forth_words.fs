bcaafb8088db20b3c8e06775ce09b5623266234e
Sam Colwell <scolwell@nfatech.com>
2018-10-14 18:06:45 -0400

    Added initial block word set to forth_words.fs

--- forth_code/forth_words.fs
+++ forth_code/forth_words.fs
@@ -46,10 +46,135 @@
         : 2constant ( d -- ) create swap , , does> dup @ swap cell+ @ ;
         : 2literal ( d -- ) swap postpone literal postpone literal ; immediate
 
+
+\ ===============================================================
+\ Support for the optional BLOCK word set
+decimal
+
+\ This code allots one single buffer in the dictionary.
+\ TODO: In the future, allow the user to specify how many buffers they
+\ want and allocate them at the end of available RAM.
+
+create blkbuffer 1024 allot ( Single 1024 byte buffer )
+variable buffblocknum 0 buffblocknum !
+variable buffstatus 0 buffstatus ! ( bit 0 = used, bit 1 = dirty )
+
+
+\ These are the required variables for the BLOCK word set.
+
+variable blk 0 blk !
+variable scr 0 scr !
+
+
+\ These deferred words need to be redirected to the user's versions
+\ before any of the BLOCK word set words are used.  Both of these
+\ words take ( buffer_address block# -- )
+defer BLOCKREAD  ( addr u -- ) 
+defer BLOCKWRITE ( addr u -- ) 
+
+\ Provide a good message if the user tries to use block words before
+\ updating BLOCKREAD or BLOCKWRITE.
+\ TODO: Provide the user a small RAMDRIVE version instead.
+: block_words_deferred
+    cr ." Please assign deferred words BLOCKREAD and BLOCKWRITE "
+    ." to your routines, eg. " cr ." ' myreadroutine IS BLOCKREAD" cr
+    abort
+;
+
+' block_words_deferred is BLOCKREAD
+' block_words_deferred is BLOCKWRITE   
+    
+
+: SAVE-BUFFERS ( -- )
+    buffstatus @ 3 = if
+        ( buffer is in use and dirty - flush buffer to storage )
+        blkbuffer buffblocknum @ BLOCKWRITE
+        ( mark buffer as clean )
+        1 buffstatus !
+    then ;
+    
+
+: BLOCK ( u -- addr ) ( blocknum -- buffer_addr )
+  ( Check for request for the current block )
+  dup buffblocknum @ = if   ( check if it's already in the buffer)
+  buffstatus @ 1 and if   ( and make sure the buffer is in use )
+    drop blkbuffer exit then then
+  ( Check if the current block is dirty )
+  buffstatus @ 3 = if
+    ( buffer is in use and dirty - flush buffer to storage )
+    blkbuffer buffblocknum @ BLOCKWRITE
+  then
+  ( Get the requested block )
+  dup buffblocknum !
+  blkbuffer swap BLOCKREAD
+  ( Mark buffer as in-use and clean )
+  1 buffstatus !
+  blkbuffer ( return block buffer address ) ;
+
+: UPDATE ( -- ) buffstatus @ 2 or buffstatus ! ;
+
+: BUFFER ( u -- addr ) ( blocknum -- buffer_addr )
+    \ Similar to BLOCK, only it doesn't read.
+    buffstatus @ 3 = if
+        ( buffer is in use and dirty - flush buffer to storage )
+        blkbuffer buffblocknum @ BLOCKWRITE
+    then
+    buffblocknum !
+    ( Mark buffer as in-use and clean )
+    1 buffstatus !
+    blkbuffer ( return block buffer address ) ;
+    
+: EMPTY-BUFFERS ( -- ) 0 buffstatus ! ;
+
+: FLUSH ( -- ) save-buffers empty-buffers ;
+
+\ Note: LOAD currently works because there is only one buffer.
+\ if/when multiple buffers are supported, we'll have to deal
+\ with the fact that it might re-load the old block into a
+\ different buffer.
+decimal
+: LOAD ( scr# - )
+  BLK @ >R                  ( We only need to save BLK    )
+  BLK !                     ( Set BLK to the new block    )
+  16 0 do                   ( Evaluate the block          )
+    BLK @ block             ( Make sure the block is here )
+    i 64 * +                ( Calculate offset to line    )
+    ( dup 64 cr type key drop )          ( DEBUG )
+    64 (  .s key drop  ) evaluate             ( Evaluate one line at a time )
+  loop
+  R> dup BLK !              ( Restore the previous block ) 
+  ?dup if block drop then ; ( and load it if not 0       )
+
+: THRU ( scr# scr# - ) 1+ swap ?do i load loop ;
+
+\ The standard says to extend EVALUATE to include storing a 0
+\ in BLK.  I'm also restoring the previous value when done.
+: evaluate blk @ >r 0 blk ! evaluate r> blk ! ;
+
+
+\ ===============================================================
+\ BLOCK Add-ons
+
+\ Provide a word to create a RAM drive with 4 blocks (0-3) in the
+\ dictionary.  RAMDRIVE takes the block number and returns the address
+\ in ram.  It provides no bounds checking.
+decimal
+: block_init_ramdrive
+s" : ramblocks create 1024 4 * allot does> swap 1024 * + ; ramblocks ramdrive
+  : blockread_ramdrive  ( addr u -- ) ramdrive swap 1024 move ;
+  : blockwrite_ramdrive ( addr u -- ) ramdrive      1024 move ;
+  ' blockread_ramdrive is blockread ' blockwrite_ramdrive is blockwrite
+  0 ramdrive 1024 4 * blank"
+  evaluate                ( Create everything  )
+;
+
+\ ===============================================================
+        
+        
 \ Splash strings. We leave these as high-level words because they are
 \ generated at the end of the boot process and signal that the other
 \ high-level definitions worked (or at least didn't crash)
-        .( Tali Forth 2 for the 65c02)
+        cr .( Tali Forth 2 for the 65c02)
         cr .( Version BETA 10. Oct 2018 )
         cr .( Copyright 2014-2018 Scot W. Stevenson)
         cr .( Tali Forth 2 comes with absolutely NO WARRANTY)
