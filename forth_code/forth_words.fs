4737d963d3541abd3c9ebd9f417080f1c1f08f61
Sam Colwell <scolwell@nfatech.com>
2018-12-20 22:10:23 -0500

    Remove assemblerized words from forth_words.fs

--- forth_code/forth_words.fs
+++ forth_code/forth_words.fs
@@ -9,45 +9,6 @@
 \ for inclusion in Ophis. This will be done automatically when "make"
 \ is run from the top level. See forth_words/README.md for details
 
-\ Output and comment. Because it still blows my mind that we can
-\ define this stuff this simply 
-\        : ( [char] ) parse 2drop ; immediate
-\        : .( [char] ) parse type ; immediate
-
-\ Flow control. Some of these could be realized with CS-ROLL and
-\ CS-PICK instead, which seems to be all the rage these days.
-\        : if postpone 0branch here 0 , ; immediate compile-only
-\        : then here swap ! ; immediate compile-only
-\        : else postpone branch here 0 , here rot ! ; immediate compile-only
-\        : repeat postpone again here swap ! ; immediate compile-only
-\        : until postpone 0branch , ; immediate compile-only
-\        : while postpone 0branch here 0 , swap ; immediate compile-only
-\        : case 0 ; immediate compile-only 
-\        : of postpone over postpone = postpone if postpone drop ; immediate compile-only 
-\        : endof postpone else ; immediate compile-only 
-\        : endcase postpone drop begin ?dup while postpone then repeat ; immediate compile-only 
-
-\ DEFER and friends. Code taken from ANS Forth specification. Some of
-\ these will be moved to assembler code in due course
-\        : defer! >body ! ;
-\        : defer@ >body @ ;
-\        : is state @ if postpone ['] postpone defer! else ' defer! then ; immediate
-\        : action-of state @ if postpone ['] postpone defer@ else ' defer@ then ; immediate
-
-\ Output definitions. Since these usually involve the user, and humans
-\ are slow, these can stay high-level for the moment. However, in this
-\ state they don't check for underflow. Based on
-\ https://github.com/philburk/pforth/blob/master/fth/numberio.fth
-\        : .r >r dup abs 0 <# #s rot sign #> r> over - spaces type ;
-\        : ud. <# #s #> type space ;
-\        : ud.r >r <# #s #> r> over - spaces type ;
-\        : d. tuck dabs <# #s rot sign #> type space ;
-\        : d.r >r tuck dabs <# #s rot sign #> r> over - spaces type ;
-
-\ Temporary high-level words. TODO convert these to assembler
-\        : 2constant ( d -- ) create swap , , does> dup @ swap cell+ @ ;
-\        : 2literal ( d -- ) swap postpone literal postpone literal ; immediate
-
 \ ===============================================================
 
 \ Extended words for the optional Search-Order wordset.
