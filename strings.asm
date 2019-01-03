707a60c28bcaae0fa7591dce167329789b42e090
Scot W. Stevenson <scot.stevenson@gmail.com>
2019-01-03 11:36:04 +0100

    More minor comment changes

--- strings.asm
+++ strings.asm
@@ -31,43 +31,43 @@ string_table:
         .word s_abc_upper, s_wid_editor, s_wid_asm, s_wid_root        ; 5-8
         .word s_see_flags, s_see_nt, s_see_xt, s_see_size             ; 9-12
 
-s_ok:           .byte " ok", 0          ; note space at beginning
-s_compiled:     .byte " compiled", 0    ; note space at beginning
-s_redefined:    .byte "redefined ", 0   ; note space at end
+s_ok:         .byte " ok", 0         ; note space at beginning
+s_compiled:   .byte " compiled", 0   ; note space at beginning
+s_redefined:  .byte "redefined ", 0  ; note space at end
 
-s_abc_lower: .byte "0123456789abcdefghijklmnopqrstuvwxyz"
-s_abc_upper: .byte "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+s_abc_lower:  .byte "0123456789abcdefghijklmnopqrstuvwxyz"
+s_abc_upper:  .byte "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-s_wid_asm:      .byte "Assembler ", 0    ; Wordlist ID 2, note space at end
-s_wid_editor:   .byte "Editor ", 0       ; Wordlist ID 1, note space at end
-s_wid_forth:    .byte "Forth ", 0        ; Wordlist ID 0, note space at end
-s_wid_root:     .byte "Root ", 0         ; Wordlist ID 3, note space at end
+s_wid_asm:    .byte "Assembler ", 0  ; Wordlist ID 2, note space at end
+s_wid_editor: .byte "Editor ", 0     ; Wordlist ID 1, note space at end
+s_wid_forth:  .byte "Forth ", 0      ; Wordlist ID 0, note space at end
+s_wid_root:   .byte "Root ", 0       ; Wordlist ID 3, note space at end
 
-s_see_flags:    .byte "flags (CO AN IM NN UF HC): ", 0
-s_see_nt:       .byte "nt: ", 0
-s_see_xt:       .byte "xt: ", 0
-s_see_size:     .byte "size (decimal): ", 0
+s_see_flags:  .byte "flags (CO AN IM NN UF HC): ", 0
+s_see_nt:     .byte "nt: ", 0
+s_see_xt:     .byte "xt: ", 0
+s_see_size:   .byte "size (decimal): ", 0
 
 
 ; ## ERROR STRINGS
 
 ; All error strings must be zero-terminated, all names start with "es_",
-; aliases with "err_" 
-; NOTE: If the string texts are changed, the test suite must be as well 
+; aliases with "err_". If the string texts are changed, the test suite must be
+; as well 
 
-.alias err_allot           0
-.alias err_badsource       1
-.alias err_compileonly     2
-.alias err_defer           3
-.alias err_divzero         4
-.alias err_noname          5
-.alias err_refill          6
-.alias err_state           7
-.alias err_syntax          8
-.alias err_underflow       9
-.alias err_negallot        10
-.alias err_wordlist        11                
-.alias err_blockwords      12
+.alias err_allot        0
+.alias err_badsource    1
+.alias err_compileonly  2
+.alias err_defer        3
+.alias err_divzero      4
+.alias err_noname       5
+.alias err_refill       6
+.alias err_state        7
+.alias err_syntax       8
+.alias err_underflow    9
+.alias err_negallot     10
+.alias err_wordlist     11                
+.alias err_blockwords   12
                 
 error_table:
         .word es_allot, es_badsource, es_compileonly, es_defer  ;  0-3
@@ -91,10 +91,10 @@ es_blockwords:  .byte "Please assign vectors BLOCK-READ-VECTOR and BLOCK-WRITE-V
 
 ; ## ENVIRONMENT STRINGS
 
-; These are used by the ENVIRONMENT? word and stored in the old string
-; format: Length byte first, then the string itself that is not rpt. not
-; zero-terminated. Note these are uppercase by ANS defintion.
-; All start with "envs_". 
+; These are used by the ENVIRONMENT? word and stored in the old string format:
+; Length byte first, then the string itself that is not rpt. not
+; zero-terminated. Note these are uppercase by ANS defintion. All start with
+; "envs_". 
 
 ; These return a single-cell number
 envs_cs:        .byte 15, "/COUNTED-STRING"
@@ -108,6 +108,9 @@ envs_max_u:     .byte 5, "MAX-U"
 envs_rsc:       .byte 18, "RETURN-STACK-CELLS"
 envs_sc:        .byte 11, "STACK-CELLS"
 envs_wl:        .byte 9, "WORDLISTS"
+
 ; These return a double-cell number
 envs_max_d:     .byte 5, "MAX-D"
 envs_max_ud:    .byte 6, "MAX-UD"
+
+; END
